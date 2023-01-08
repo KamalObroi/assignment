2afd2687dc132a69298b9d67d10d742a486fbca8
Kamal <kamal.oberoi@aajtak.com>
2023-01-09 00:06:52 +0530

    Assignment Block Update, Service Update

--- web/modules/custom/assignment/js/location_block.js
+++ web/modules/custom/assignment/js/location_block.js
@@ -1,14 +1,11 @@
 Drupal.behaviors.assignment = {
-   attach: function (context, drupalSettings) {
-   console.log(context);
-   console.log(drupalSettings);
+   attach: function (context, drupalSettings) {   
    jQuery.ajax({
       url: '/specbee/web/getupdatedDateTime',
       method: 'get',
       data: {},
       contentType: 'application/json',
-      success: function(jsonresponse){
-         console.log(jsonresponse);
+      success: function(jsonresponse){         
          jQuery('#country').text(jsonresponse.location.country);
          jQuery('#city').text(jsonresponse.location.city);
          jQuery('#timezone').text(jsonresponse.location.timezone);
